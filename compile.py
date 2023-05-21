911b398af78f52f5cdebd91ccc78603c3ae6c3e2
kai-bo <kai.buckhalter@gmail.com>
2023-05-21 04:35:41 -0400

    Project Cleaning; Checked Requirements

--- compile.py
+++ compile.py
@@ -1,27 +1,27 @@
 # This program pulls from preset locations and an exported bookmarks file
 # in order to save the song information into a json file for later analysis
-# Preset locations: '{KAISTUFF}:\Creating\Music\analog',
-#                   '{KAISTUFF}:\Creating\Music\beepbox'
-# Bookmarks file must be named 'bookmarks.html'
+# Preset locations: "{KAISTUFF}:\Creating\Music\analog",
+#                   "{KAISTUFF}:\Creating\Music\beepbox"
+# Bookmarks file must be named "bookmarks.html"
 
 from functools import total_ordering
 from json import dumps
 from os import listdir
 from os.path import getctime, getsize, samefile, basename, splitext
 from bs4 import BeautifulSoup as bs
+from selenium.webdriver import Chrome, ActionChains
 from selenium.webdriver import ChromeOptions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver import Chrome, ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from tqdm import tqdm
 from mutagen.wave import WAVE
 from mutagen.mp3 import MP3
 from wmi import WMI
 
-supported_audio_extensions = ('.wav', '.mp3')
+supported_audio_extensions = (".wav", ".mp3")
 
 # Holds data about the song file such as the path, name, and length
 @total_ordering
@@ -35,16 +35,16 @@ class SongFile:
         self.audio_format: str = path_splitext[1]
 
         if not (self.audio_format in supported_audio_extensions):
-            raise ValueError(f'Unsupported audio format: {self.audio_format}')
+            raise ValueError(f"Unsupported audio format: {self.audio_format}")
         
         self._init_song_metadata()
         self._init_file_metadata()
     
     # Initialize metadata relating to the music
     def _init_song_metadata(self) -> None:
-        if self.audio_format == '.wav':
+        if self.audio_format == ".wav":
             self._mutagen_handle: WAVE = WAVE(self.file_path)
-        elif self.audio_format == '.mp3':
+        elif self.audio_format == ".mp3":
             self._mutagen_handle: MP3 = MP3(self.file_path)
         
         self.length: float = self._mutagen_handle.info.length
@@ -58,8 +58,8 @@ class SongFile:
         return self.name
     
     def _is_valid_operand(self, other: object) -> bool:
-        return (hasattr(other, 'name')
-                and hasattr(other, 'creation_time'))
+        return (hasattr(other, "name")
+                and hasattr(other, "creation_time"))
     
     # Two song files are only equivalent if they are the same file (same path)
     def __eq__(self, other: object) -> bool:
@@ -84,11 +84,11 @@ class Song:
         self.creation_time = creation_time
     
     def __str__(self) -> str:
-        return f'{self.creation_time}\t{self.length:<4.1f} {self.name}'
+        return f"{self.creation_time}\t{self.length:<4.1f} {self.name}"
     
     def _is_valid_operand(self, other: object) -> bool:
-        return (hasattr(other, 'name')
-                and hasattr(other, 'creation_time'))
+        return (hasattr(other, "name")
+                and hasattr(other, "creation_time"))
     
     # Two songs are equivalent if they have the same name
     def __eq__(self, other: object) -> bool:
@@ -109,7 +109,7 @@ class SongCompiler:
     # Get drive letter of KAISTUFF flashdrive
     @classmethod
     def get_kaistuff_drive_letter(cls):
-        drive_name = "KAISTUFF"
+        drive_name = 'KAISTUFF'
         drive_type = 2
         c = WMI()
         matching_drives = list(filter(lambda disk:
@@ -118,10 +118,10 @@ class SongCompiler:
                                       c.Win32_LogicalDisk()))
         
         if len(matching_drives) == 0:
-            print(f'Unable to locate {drive_name} drive')
+            print(f"Unable to locate {drive_name} drive")
             return None
         elif len(matching_drives) > 1:
-            print(f'Ambiguous, multiple drives found titled {drive_name}')
+            print(f"Ambiguous, multiple drives found titled {drive_name}")
             return None
         
         return matching_drives[0].Caption
@@ -140,8 +140,8 @@ class SongCompiler:
         if drive_letter is None:
             return []
 
-        beepbox_path = f'{drive_letter}\\Creating\\Music\\beepbox'
-        analog_path = f'{drive_letter}\\Creating\\Music\\analog'
+        beepbox_path = f"{drive_letter}\\Creating\\Music\\beepbox"
+        analog_path = f"{drive_letter}\\Creating\\Music\\analog"
 
         beepbox_filenames = listdir(beepbox_path)
         analog_filenames = listdir(analog_path)
@@ -149,10 +149,10 @@ class SongCompiler:
         all_filepaths = []
 
         for file in beepbox_filenames:
-            all_filepaths.append(beepbox_path + '\\' + file)
+            all_filepaths.append(beepbox_path + "\\" + file)
 
         for file in analog_filenames:
-            all_filepaths.append(analog_path + '\\' + file)
+            all_filepaths.append(analog_path + "\\" + file)
         
         song_file_paths = filter(lambda file:
                                  SongCompiler.is_audio_file(file),
@@ -171,22 +171,22 @@ class SongCompiler:
     def get_bookmarked_songs_info(cls) -> tuple[list[str], list[str],
                                                 list[float]]:
         
-        bookmarks = 'bookmarks.html'
+        bookmarks = "bookmarks.html"
         names = []
         links = []
         times = []
 
-        soup = bs(open(bookmarks).read())
+        soup = bs(open(bookmarks).read(), features="lxml")
         songs_header = soup(lambda tag:
-                            tag.name == 'h3' and tag.text == 'Songs')[0]
-        songs_list = songs_header.find_parent().next_sibling.find_all('a')
+                            tag.name == "h3" and tag.text == "Songs")[0]
+        songs_list = songs_header.find_parent().next_sibling.find_all("a")
 
         # Retrieve song information and link for each item in Songs
         for link in songs_list:
             names.append(link.text)
-            links.append('https://jummbus.bitbucket.io/#'
-                         + link['href'].split('#')[1])
-            times.append(float(link['add_date']))
+            links.append("https://jummbus.bitbucket.io/#"
+                         + link["href"].split("#")[1])
+            times.append(float(link["add_date"]))
 
         return (names, links, times)
 
@@ -196,10 +196,8 @@ class SongCompiler:
         songs = []
         
         # Initialize web driver
-        webdriver_service = Service('C:/Program Files (x86)/chromedriver.exe')
+        webdriver_service = Service("C:/Program Files (x86)/chromedriver.exe")
         options = ChromeOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
         driver = Chrome(service=webdriver_service, options=options)
 
         for i, _ in tqdm(enumerate(info[0])):
@@ -209,7 +207,7 @@ class SongCompiler:
 
             try:
                 # Click file menu
-                xpath = "//div[@class='selectContainer menu file']/select[1]"
+                xpath = '//div[@class="selectContainer menu file"]/select[1]'
                 file_dropdown = WebDriverWait(driver, 10).until(
                     EC.visibility_of_element_located((By.XPATH, xpath)))
                 ActionChains(driver).move_to_element(file_dropdown).click()\
@@ -217,22 +215,22 @@ class SongCompiler:
 
                 # Select export option
                 select = Select(file_dropdown)
-                select.select_by_value('export')
+                select.select_by_value("export")
 
                 # Get song length
-                xpath = "//div[@class='prompt noSelection']"
+                xpath = '//div[@class="prompt noSelection"]'
                 export_menu = WebDriverWait(driver, 10).until(
                     EC.visibility_of_element_located((By.XPATH, xpath)))
-                xpath = ".//div[2]//div[1]"
+                xpath = './/div[2]//div[1]'
                 length_element = export_menu.find_element(By.XPATH, xpath)
-                mins, secs = length_element.text.split(':')
+                mins, secs = length_element.text.split(":")
                 length = float(mins) * 60 + float(secs)
                 
                 song = Song(info[0][i], length, info[2][i])
                 songs.append(song)
 
             except:
-                print(f'Failed to retrieve song length of {info[0][i]}')
+                print(f"Failed to retrieve song length of {info[0][i]}")
         
         driver.quit()
         
@@ -280,9 +278,9 @@ class SongCompiler:
 
         json_string = dumps(songs, default=lambda o: o.__dict__, indent=4,
                             sort_keys=True)
-        with open('songs.json', 'w') as f:
+        with open("songs.json", "w") as f:
             f.write(json_string)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     SongCompiler.save_songs_json()
--- main.py
+++ main.py
@@ -47,30 +47,30 @@ def graph_histogram(music_histogram: list[MusicThroughput], bin_size_days, years
         # Leave every other label blank for spacing
         if i % skip_mod == 0:
             # labels are in the format MM/YY
-            lbls.append(f'{bin_data.start_time.month}/'
-                        + f'{bin_data.start_time.day}/'
-                        + f'{bin_data.start_time.year - 2000}')
+            lbls.append(f"{bin_data.start_time.month}/"
+                        + f"{bin_data.start_time.day}/"
+                        + f"{bin_data.start_time.year - 2000}")
         else:
-            lbls.append('')
+            lbls.append("")
     
     # Display overall statistics about histogram
-    max_start = f'{max_tp.start_time.month}/{max_tp.start_time.day}/'\
-        + f'{max_tp.start_time.year - 2000}'
-    max_end = f'{max_tp.end_time.month}/{max_tp.end_time.day}/'\
-        + f'{max_tp.end_time.year - 2000}'
-    print(f'Total music made between: {total_time:.1f} seconds')
-    print(f'Maximum music made between {max_start} - {max_end}: '
-          + f'{max_tp.throughput:.1f} seconds')
+    max_start = f"{max_tp.start_time.month}/{max_tp.start_time.day}/"\
+        + f"{max_tp.start_time.year - 2000}"
+    max_end = f"{max_tp.end_time.month}/{max_tp.end_time.day}/"\
+        + f"{max_tp.end_time.year - 2000}"
+    print(f"Total music made between: {total_time:.1f} seconds")
+    print(f"Maximum music made between {max_start} - {max_end}: "
+          + f"{max_tp.throughput:.1f} seconds")
 
     # Graph histogram
-    df = pd.DataFrame({'time': t, 'throughput': m})
+    df = pd.DataFrame({"time": t, "throughput": m})
 
-    sns.set_style('whitegrid')
-    ax = sns.barplot(data=df, x='time', y='throughput', orient='v', width=1.0)
+    sns.set_style("whitegrid")
+    ax = sns.barplot(data=df, x="time", y="throughput", orient="v", width=1.0)
 
-    ax.set(xlabel=f'Time (tΔ = {bin_size_days:.1f} days)',
-           ylabel='Total music made (seconds)',
-           title=f'Total Music Made Per Every {bin_size_days:.1f} Days')
+    ax.set(xlabel=f"Time (tΔ = {bin_size_days:.1f} days)",
+           ylabel="Total music made (seconds)",
+           title=f"Total Music Made Per Every {bin_size_days:.1f} Days")
 
     plt.xticks(range(len(t)), labels=lbls, rotation=-60)
     plt.show()
@@ -78,21 +78,21 @@ def graph_histogram(music_histogram: list[MusicThroughput], bin_size_days, years
 # Compiles all songs into a histogram, depicting when (over some time delta)
 # and how much music in total over said time delta
 def main():
-    expected_parameters = '(float)bin_size_days, [(float)years_to_show]'
+    expected_parameters = "(float)bin_size_days, [(float)years_to_show]"
     try:
         bin_size_days = float(argv[1])
     except ValueError:
-        print('Mismatch parameter type.\nExpected: ' + expected_parameters)
+        print("Mismatch parameter type.\nExpected: " + expected_parameters)
         return
     except IndexError:
-        print(f'Received {len(argv) - 1} parameters.\n'
-              + 'Expected: ' + expected_parameters)
+        print(f"Received {len(argv) - 1} parameters.\n"
+              + "Expected: " + expected_parameters)
         return
 
     try:
         years_to_show = float(argv[2])
     except ValueError:
-        print('Mismatch parameter type.\nExpected: ' + expected_parameters)
+        print("Mismatch parameter type.\nExpected: " + expected_parameters)
         return
     except IndexError:
         years_to_show = datetime.now().year
@@ -100,16 +100,16 @@ def main():
     songs = []
 
     # Load from songs.json into songs list
-    with open('songs.json') as f:
+    with open("songs.json") as f:
         data = load(f)
         for obj in data:
-            songs.append(Song(obj['name'], obj['length'],
-                              obj['creation_time']))
+            songs.append(Song(obj["name"], obj["length"],
+                              obj["creation_time"]))
 
     music_histogram = MusicThroughput.create_histogram(songs, bin_size_days)
 
     graph_histogram(music_histogram, bin_size_days, years_to_show)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
